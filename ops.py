d9a89632668ac4eb31d0bc6ddfff9f1052e9c9eb
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-09-28 12:53:41 +0900

    update

--- ops.py
+++ ops.py
@@ -172,8 +172,8 @@ def deconv2d(inputs, filters, kernel_size, strides, data_format, name="deconv2d"
         strides = [1] + [1] + strides if data_format_abbr == "NCHW" else [1] + strides + [1]
 
         output_shape = tf.shape(inputs) * strides
-        output_shape = (tf.concat([output_shape[:1], [filters], output_shape[2:4]]) if data_format_abbr == "NCHW" else
-                        tf.concat([output_shape[:1], output_shape[1:3], [filters]]))
+        output_shape = (tf.concat([output_shape[0:1], [filters], output_shape[2:4]], axis=0) if data_format_abbr == "NCHW" else
+                        tf.concat([output_shape[0:1], output_shape[1:3], [filters]], axis=0))
 
         inputs = tf.nn.conv2d_transpose(
             value=inputs,
